c6080dfd7ae386c3a481853d946a239705bef5a2
t.jetzinger <t.jetzinger@gmail.com>
2015-04-22 12:58:30 +0200

    - Added simple (navigation drawer) ionic openHAB project

--- Source/openHABit/platforms/android/assets/www/cordova_plugins.js
+++ Source/openHABit/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,26 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.device": "0.3.0",
+    "org.apache.cordova.console": "0.2.13",
+    "com.ionic.keyboard": "1.0.4"
+}
 // BOTTOM OF METADATA
 });
