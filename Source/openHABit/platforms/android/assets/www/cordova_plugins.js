046946cd2604a9fd4aa090836911764597f3ad92
t.jetzinger <t.jetzinger@gmail.com>
2015-04-22 10:42:27 +0200

    - removed unused cordova plugins - inserted simple drawer in cordova app

--- Source/openHABit/platforms/android/assets/www/cordova_plugins.js
+++ Source/openHABit/platforms/android/assets/www/cordova_plugins.js
@@ -1,26 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
-        "id": "cordova-plugin-vibration.notification",
-        "merges": [
-            "navigator.notification",
-            "navigator"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-plugin-vibration": "1.0.1-dev",
-    "cordova-plugin-splashscreen": "2.0.1-dev"
-}
+{}
 // BOTTOM OF METADATA
 });
