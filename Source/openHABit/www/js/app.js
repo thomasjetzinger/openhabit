8754b0c74a12006646be794c1511aa81bbabd1fc
t.jetzinger <t.jetzinger@gmail.com>
2015-05-05 14:53:41 +0200

    - rename main mobile to openHabitModule

--- Source/openHABit/www/js/app.js
+++ Source/openHABit/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'openHabit' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var controllerModule = angular.module('openHABit', ['ionic', 'ngMaterial', 'ngMdIcons', 'sitemapServices'])
+var openHabitModule = angular.module('openHABit', ['ionic', 'ngMaterial', 'ngMdIcons', 'sitemapServices'])
 
 
     .run(function ($ionicPlatform) {
