284acf552bb4993b423ee69e432f8cd150861cf6
t.jetzinger <t.jetzinger@gmail.com>
2015-05-20 10:42:02 +0200

    - add setItem function

--- Source/openHABit/www/js/sitemapcontent_service.js
+++ Source/openHABit/www/js/sitemapcontent_service.js
@@ -33,6 +33,34 @@ var sitemapContentModule = angular.module('SiteMapContentServiceModule', [])
             return result;
         }
 
+        function getSitemap(_id) {
+            var sitemap = sitemaps.filter(function (obj) {
+                if (_id.indexOf(obj.id) >= 0) {
+                    return obj;
+                }
+            });
+
+            if(angular.isArray(sitemap))
+                return sitemap[0];
+            else
+                return sitemap;
+        }
+
+        /**
+         * Gets the full id (e.g. app.demo.0000 from 0000)
+         * @param _id
+         */
+        function getFullId(_id) {
+            if(_id in currentSitemap.widgetCollection == false) {
+                _id = currentSitemap.widgetCollection.filter(function (key) {
+                    if (key.indexOf(_id) >= 0)
+                        return _id;
+                });
+            }
+
+            return _id;
+         }
+
 
 
         return {
@@ -51,27 +79,33 @@ var sitemapContentModule = angular.module('SiteMapContentServiceModule', [])
             //
             getItem: function (_id) {
                 console.log("SiteMapContentService getItem called for id " + _id);
-                var result_sitemap = sitemaps.filter(function (obj) {
-                    if (_id.indexOf(obj.id) >= 0) {
-                        return obj
-                    }
-                });
+                var result_sitemap = getSitemap(_id);
 
-                var  flat_items = flatternWidgets(result_sitemap[0].widgetCollection[_id]);
-                if (result_sitemap.length > 0 && _id in result_sitemap[0].widgetCollection) {
-                    return {
-                        label: result_sitemap[0].widgetCollection[_id].label,
-                        items: flat_items,
-                        linkedState: _id
-                    };
+                if (result_sitemap) {
+
+                    var flat_items = flatternWidgets(result_sitemap.widgetCollection[_id]);
+                    if (_id in result_sitemap.widgetCollection) {
+                        return {
+                            label: result_sitemap.widgetCollection[_id].label,
+                            items: flat_items,
+                            linkedState: _id
+                        };
+                    }
                 }
+                else
+                    console.error('no sitemap for id ' + _id);
             },
 
-
-
+            setItem: function(_id, _widgets) {
+                _id = getFullId(_id);
+                currentSitemap.widgetCollection[_id] = _widgets;
+            },
 
             setSitemaps: function (_sitemaps) {
                 sitemaps = _sitemaps;
+
+                //TODO remove this
+                currentSitemap = sitemaps[0];
             },
 
             getSitemaps: function () {
@@ -85,6 +119,5 @@ var sitemapContentModule = angular.module('SiteMapContentServiceModule', [])
             getCurrentSitemap: function () {
                 return currentSitemap;
             }
-
         }
     });
