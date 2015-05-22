c9c3c470ce675e2df0466f518d15546c37bfae25
t.jetzinger <t.jetzinger@gmail.com>
2015-05-22 10:50:29 +0200

    - move websockets to rootscope

--- Source/openHABit/www/js/main_controller.js
+++ Source/openHABit/www/js/main_controller.js
@@ -1,4 +1,4 @@
-openHabitModule.controller('mainController', function ($scope, $state, sitemapContent, sitemapName, pageTitle, Item, Page, ModelService, $websocket, $localStorage) {
+openHabitModule.controller('mainController', function ($rootScope, $scope, $state, sitemapContent, sitemapName, pageTitle, Item, Page, ModelService, $websocket, $localStorage) {
 
     $scope.pageId = sitemapName.substr(sitemapName.lastIndexOf(".") + 1);
     var page = Page($scope.pageId).query();
@@ -25,46 +25,49 @@ openHabitModule.controller('mainController', function ($scope, $state, sitemapCo
 
     $scope.$on('$ionicView.enter', function() {
         // Code you want executed every time view is opened
-        console.log(sitemapName+ ' Opened!')
-        if($scope.ws === undefined){
-            //todo gen unique tracking id
-            console.log('ws://'+$localStorage.url+'/rest/sitemaps/'+ModelService.getCurrentSitemapId()+'/'+$scope.pageId+'?X-Atmosphere-tracking-id=a5c1f99f-e88b-3266-cbce-f461bfbfe14d&X-Atmosphere-Framework=0.9&X-Atmosphere-Transport=websocket&X-Cache-Date=0&Accept=application%2Fjson');
+        console.log(sitemapName+ ' entered!');
 
-            $scope.ws = $websocket.$new('ws://'+$localStorage.url+'/rest/sitemaps/'+ModelService.getCurrentSitemapId()+'/'+$scope.pageId+'?X-Atmosphere-tracking-id=a5c1f99f-e88b-3266-cbce-f461bfbfe14d&X-Atmosphere-Framework=0.9&X-Atmosphere-Transport=websocket&X-Cache-Date=0&Accept=application%2Fjson'); // instance of ngWebsocket, handled by $websocket service
-
-            $scope.ws.$on('$open', function () {
-                console.log(sitemapName+' Websocket is open!');
-
-            });
-
-
-            $scope.ws.$on('$message', function (data) {
-                console.log('$message The websocket server has sent the following data:');
-                console.log(data);
-
-                if (data.widget) {
-                    ModelService.setItem(data.widget.widgetId, data.widget);
-                    $scope.$apply(function () {
-                        $scope.sitemapData = ModelService.getItem($scope.pageId);
-                    });
-
-                }
-            });
-
-            $scope.ws.$on('$close', function () {
-                console.log(sitemapName + 'Websocket closed!');
-            });
-
-
+        if($rootScope.ws) {
+            console.log('status: ' + $rootScope.ws.$status());
+            console.log("close websocket");
+            $rootScope.ws.$un('$message');
+            //$rootScope.ws.$close();
         }
 
+
+        //todo gen unique tracking id
+        console.log('ws://'+$localStorage.url+'/rest/sitemaps/'+ModelService.getCurrentSitemapId()+'/'+$scope.pageId+'?X-Atmosphere-tracking-id=a5c1f99-e88b-3266-cbce-f461bfbfe14d&X-Atmosphere-Framework=0.9&X-Atmosphere-Transport=websocket&X-Cache-Date=0&Accept=application%2Fjson');
+
+        $rootScope.ws = $websocket.$new('ws://'+$localStorage.url+'/rest/sitemaps/'+ModelService.getCurrentSitemapId()+'/'+$scope.pageId+'?X-Atmosphere-Framework=0.9&X-Atmosphere-Transport=websocket&X-Cache-Date=0&Accept=application%2Fjson'); // instance of ngWebsocket, handled by $websocket service
+
+        $rootScope.ws.$on('$open', function () {
+            console.log(sitemapName+' Websocket is open!');
+        });
+
+        $rootScope.ws.$on('widget', function (message) {
+            console.log('widget' + message); // it prints 'parrot data'
+        });
+
+        $rootScope.ws.$on('$message', function (data) {
+            console.log('$message The websocket server has sent the following data:');
+            console.log(data);
+
+            if (data.widget) {
+                ModelService.setItem(data.widget.widgetId, data.widget);
+                $scope.$apply(function () {
+                    $scope.sitemapData = ModelService.getItem($scope.pageId);
+                });
+
+            }
+        });
+
+        $rootScope.ws.$on('$close', function () {
+            console.log(sitemapName + 'Websocket closed!');
+        });
     });
 
     $scope.$on('$ionicView.leave', function() {
         // Code you want executed every time view is opened
-        console.log(sitemapName+' exit!')
-        if($scope.ws !== undefined) {
-            $scope.ws.$close();
-        }
+        console.log(sitemapName + ' exit!')
     });
 });
